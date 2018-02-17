aecc8172db9253b12b8fe4c13cd094f6097380ed
Peggy <hikaru707@gmail.com>
2018-02-17 14:45:06 +0800

    加上view

---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>Sample App</title>
+</head>
+<body>
+    <h1>關於頁</h1>
+</body>
+</html>
---
+++ resources/views/static_pages/help.blade.php
@@ -0,0 +1,8 @@
+<html>
+<head>
+    <title>Sample App</title>
+</head>
+<body>
+    <h1>幫助頁</h1>
+</body>
+</html>
