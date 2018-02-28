5f0b1f5a36b808b08893b4aee2b7336796fc9f45
Peggy <hikaru707@gmail.com>
2018-02-28 16:13:10 +0800

    seed 時 user 1 設為管理員

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -19,6 +19,7 @@ class UsersTableSeeder extends Seeder
         $user->name = 'admin';
         $user->email = 'abc@gmail.com';
         $user->password = bcrypt('password');
+        $user->is_admin = true;
         $user->save();
     }
 }
